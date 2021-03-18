e4b436dabfa44698f790f4071395a39e294e7859
giovanniardenghi <36991376+giovanniardenghi@users.noreply.github.com>
2021-03-18 09:43:29 +0100

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-sudo sed -i '1s/^/openssl_conf = default_conf\n /' /etc/ssl/openssl.cnf
-echo something | sudo tee /etc/ssl/openssl.cnf
+
 # Get and save latest data
 curl https://www.epicentro.iss.it/coronavirus/open-data/calcolo_rt_italia.zip --output data/calcolo_rt_italia.zip 
 
